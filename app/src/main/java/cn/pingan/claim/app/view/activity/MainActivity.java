70ccae65700f7d5f4a2415740dfd2b4294951231
Kezy <ke11839@163.com>
2020-01-20 13:28:01 +0800

    [Mod] 视频播放框架部分

--- app/src/main/java/cn/pingan/claim/app/view/activity/MainActivity.java
+++ app/src/main/java/cn/pingan/claim/app/view/activity/MainActivity.java
@@ -8,6 +8,10 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+
+import cn.jzvd.Jzvd;
+import cn.jzvd.view.MyJzvdStd;
 import cn.pingan.claim.app.R;
 import cn.pingan.claim.app.base.common.net.ICallBack;
 import cn.pingan.claim.app.model.bean.InitConfigResponse;
@@ -18,10 +22,20 @@ public class MainActivity extends AppCompatActivity {
 
     private Button button, button2;
     private TextView msg;
+
+    MyJzvdStd myJzvdStd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        myJzvdStd = findViewById(R.id.jz_video);
+        myJzvdStd.setUp("http://jzvd.nathen.cn/342a5f7ef6124a4a8faf00e738b8bee4/cf6d9db0bd4d41f59d09ea0a81e918fd-5287d2089db37e62345123a1be272f8b.mp4"
+                , "饺子快长大");
+        Glide.with(this).load("http://jzvd-pic.nathen.cn/jzvd-pic/1bb2ebbe-140d-4e2e-abd2-9e7e564f71ac.png").into(myJzvdStd.thumbImageView);
+
 
         button = findViewById(R.id.btn1);
         button2 = findViewById(R.id.btn2);
@@ -70,5 +84,19 @@ public class MainActivity extends AppCompatActivity {
                 msg.setTextColor(Color.RED);
             }
         });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Jzvd.releaseAllVideos();
+        myJzvdStd.onStatePause();
+    }
+    @Override
+    public void onBackPressed() {
+        if (Jzvd.backPress()) {
+            return;
+        }
+        super.onBackPressed();
     }
 }
